82560ae73afe1f80e3a5b6d7d2548e1fc81e2cda
Sean Camden <sean@seancamden.com>
2010-12-03 20:57:48 -0800

    Added admin menu.

--- listusers.php
+++ listusers.php
@@ -25,13 +25,13 @@ License: GPL2
 */
 add_action('admin_menu', 'list_users_menu');
 
-function my_plugin_menu() {
+function list_users_menu() {
 
-  add_options_page('My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options');
+  add_options_page('List Users Options', 'List Users', 'manage_options', 'list-users-settings', 'list_users_options');
 
 }
 
-function my_plugin_options() {
+function list_users_options() {
 
   if (!current_user_can('manage_options'))  {
     wp_die( __('You do not have sufficient permissions to access this page.') );
