75de9fbe4e137e523bc078135b3a6bcc6a82a282
Sean Camden <sean@seancamden.com>
2010-12-03 17:21:58 -0800

    The main PHP file. Nothing much in it yet.

---
+++ listusers.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: List Users
+Plugin URI: https://github.com/wagonlips/listusers
+Description: A plugin to list specific users in WordPress and show the posts they've written.
+             Using a widget, specific names selected by an admin will be displayed in a sidebar, 
+             when clicked, the category of authored pages will be displayed.
+Version: 0.1
+Author: Sean Camden
+Author URI: http://computerdemon.com
+License: GPL2
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+?>
+
