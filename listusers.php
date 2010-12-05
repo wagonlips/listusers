a56fb1c46dd91fef53d85f0107b430300e0410df
Sean Camden <sean@seancamden.com>
2010-12-04 22:18:18 -0800

    Going all object oriented.

--- listusers.php
+++ listusers.php
@@ -23,15 +23,42 @@ License: GPL2
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-function widget_list_users_register() {
-    function widget_list_users($args) {
-      extract($args);
-      echo $before_widget; 
-      echo $before_title . 'List Users Widget' . $after_title; 
-      ?> Hello, everyone!  <?php 
-      echo $after_widget;    
-    }
-    register_sidebar_widget('List Users Widget','widget_list_users');
-}
 add_action('init', widget_list_users_register);
+function widget_list_users_register() {
+  register_widget('List_Users');
+}
+class List_Users extends WP_Widget {
+  function List_Users() {
+    /* Widget settings. */
+    $widget_ops = array( 'classname' => 'listusers', 'description' => __('A widget to display selected users.', 'listusers') );
+
+    /* Widget control settings. */
+    $control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'list-users-widget' );
+
+    /* Create the widget. */
+    $this->WP_Widget( 'list-users-widget', __('List Users', 'listusers'), $widget_ops, $control_ops );
+  }
+  /* Display the widget. */
+  function widget( $args, $instance ) {
+    extract( $args );
+
+    /* Our variables from the widget settings. */
+    $title = apply_filters('widget_title', $instance['title'] );
+    $name = $instance['name'];
+
+    /* Before widget (defined by themes). */
+    echo $before_widget;
+
+    /* Display the widget title if one was input (before and after defined by themes). */
+    if ( $title )
+      echo $before_title . $title . $after_title;
+
+    /* Display name from widget settings if one was input. */
+    if ( $name )
+      printf( '<p>' . __('These are the names %1$s.', 'example') . '</p>', $name );
+
+    /* After widget (defined by themes). */
+    echo $after_widget;
+  }
+}
 ?>
